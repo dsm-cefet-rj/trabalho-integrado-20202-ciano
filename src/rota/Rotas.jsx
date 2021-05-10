eff96b418ba8ff8a9abfe8f43314ccb397986de7
Marcelo Dias <48864177+Aegir9@users.noreply.github.com>
2021-05-10 18:45:32 -0300

    Redirecionar usuários não autenticados ou não autorizados.
    Criação da lógica de redirecionamento de usuários não autenticados ou não autorizados de rotas privadas do sistema.

--- src/rota/Rotas.jsx
+++ src/rota/Rotas.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import ConsultarEmprestimo from '../components/emprestimo/consultar/ConsultarEmprestimo';
 import ConsultarEmprestimoCompleto from '../components/emprestimo/consultar/ConsultarEmprestimoCompleto';
 import ConsultarEmprestimoMatricula from '../components/emprestimo/consultar/ConsultarEmprestimoMatricula';
@@ -15,6 +16,7 @@ import Home from '../components/geral/home/Home';
 import CadastrarUsuario from '../components/geral/usuario/CadastrarUsuario';
 import Login from '../components/geral/usuario/Login';
 import MenuInicialBibliotecario from '../components/geral/usuario/MenuInicialBibliotecario';
+import MenuInicial from '../components/geral/usuario/MenuInicial';
 import MenuInicialUsuario from '../components/geral/usuario/MenuInicialUsuario';
 import AtualizarLivro from '../components/livro/AtualizarLivro';
 import CadastrarLivro from '../components/livro/CadastrarLivro';
@@ -26,46 +28,126 @@ import RelatorioDevolucoesLivros from '../components/relatorio/RelatorioDevoluco
 import RelatorioLivrosEmprestado from '../components/relatorio/RelatorioLivrosEmprestado';
 
 function Rotas() {
+	const login = useSelector(state => state.logins);
+
+	function PrivateRoute({ children, ...rest }) {
+		console.log(children);
+		console.log(rest);
+		return (
+			<Route {...rest} render={({ location }) => {
+				if (login.status === 'logged_in') {
+					if (typeof rest.authorize !== 'undefined') {
+						let UserAuth = login.currentUserAuth;
+						let authorize = rest.authorize;
+
+						// Verifica se a categoria do usuário autenticado está contido na categoria exigida pela autorização da rota.
+						if (authorize.filter(categoria => categoria === UserAuth.categoria).length !== 0)
+							return children;
+						else
+							return <Redirect to={{
+								pathname: '/menu',
+								state: { from: location }
+							}} />
+					}
+					else
+						return children;
+				}
+				else
+					return <Redirect to={{
+						pathname: '/login',
+						state: { from: location }
+					}} />
+			}}
+			/>
+		)
+	}
+
 	return (
 		<Router>
 			<Switch>
-				<Route path="/" component={Home} exact />		
+				<Route path="/" component={Home} exact />
 				<Route path="/login" component={Login} />
-				<Route path="/menu/bibliotecario" component={MenuInicialBibliotecario} />
-				
+				<PrivateRoute path="/menu/bibliotecario" authorize={['bibliotecario']} >
+					<MenuInicialBibliotecario />
+				</PrivateRoute>
+
 				<Route path="/menu/usuario" component={MenuInicialUsuario} />
 
+				<PrivateRoute path="/menu" >
+					<MenuInicial />
+				</PrivateRoute>
+
 				{/* ---------------- Usuário ---------------- */}
-				<Route path="/usuario/cadastrar" component={CadastrarUsuario} />
+				<PrivateRoute path="/usuario/cadastrar" authorize={['bibliotecario']} >
+					<CadastrarUsuario />
+				</PrivateRoute>
 
 				{/* --------------- Emprestimo --------------- */}
-				<Route path="/emprestimo" component={ManterEmprestimo} exact />
-				<Route path="/emprestimo/registrar" component={RegistrarEmprestimo} />
+				<PrivateRoute path="/emprestimo" exact >
+					<ManterEmprestimo />
+				</PrivateRoute>
 
-				<Route path="/emprestimo/encerrar" component={EncerrarEmprestimo} exact />
-				<Route path="/emprestimo/encerrar/:id" component={EncerrarEmprestimoConsulta} exact />
-				<Route path="/emprestimo/encerrar/:id/confirmacao" component={EncerrarEmprestimoConfirmacao} />
+				<PrivateRoute path="/emprestimo/registrar" authorize={['bibliotecario']} >
+					<RegistrarEmprestimo />
+				</PrivateRoute>
 
-				<Route path="/emprestimo/consultar" component={ConsultarEmprestimoMatricula} exact />
-				<Route path="/emprestimo/consultar/:id" component={ConsultarEmprestimo} exact />
-				<Route path="/emprestimo/consultar/:id/confirmacao" component={ConsultarEmprestimoCompleto} />
+				<PrivateRoute path="/emprestimo/encerrar" authorize={['bibliotecario']} exact >
+					<EncerrarEmprestimo />
+				</PrivateRoute>
+				<PrivateRoute path="/emprestimo/encerrar/:id" authorize={['bibliotecario']} exact >
+					<EncerrarEmprestimoConsulta />
+				</PrivateRoute>
+				<PrivateRoute path="/emprestimo/encerrar/:id/confirmacao" authorize={['bibliotecario']} >
+					<EncerrarEmprestimoConfirmacao />
+				</PrivateRoute>
 
-				<Route path="/emprestimo/renovar" component={RenovarEmprestimo} exact />
-				<Route path="/emprestimo/renovar/:id" component={RenovarEmprestimoConsulta} exact />
-				<Route path="/emprestimo/renovar/:id/confirmacao" component={RenovarEmprestimoConfirmacao} />
+				<PrivateRoute path="/emprestimo/consultar" exact >
+					<ConsultarEmprestimoMatricula />
+				</PrivateRoute>
+				<PrivateRoute path="/emprestimo/consultar/:id" exact >
+					<ConsultarEmprestimo />
+				</PrivateRoute>
+				<PrivateRoute path="/emprestimo/consultar/:id/confirmacao" >
+					<ConsultarEmprestimoCompleto />
+				</PrivateRoute>
+
+				<PrivateRoute path="/emprestimo/renovar" exact >
+					<RenovarEmprestimo />
+				</PrivateRoute>
+				<PrivateRoute path="/emprestimo/renovar/:id" exact >
+					<RenovarEmprestimoConsulta />
+				</PrivateRoute>
+				<PrivateRoute path="/emprestimo/renovar/:id/confirmacao" >
+					<RenovarEmprestimoConfirmacao />
+				</PrivateRoute>
 
 				{/* ---------------- Relatório ---------------- */}
-				<Route path="/relatorio" component={ManterRelatorio} exact />
-				<Route path="/relatorio/devolucoes" component={RelatorioDevolucoesLivros} exact />
-				<Route path="/relatorio/emprestimos" component={RelatorioLivrosEmprestado} />
-				<Route path="/relatorio/emprestimo/:id" component={InformacoesDetalhadasEmprestimo} />
+				<PrivateRoute path="/relatorio" authorize={['bibliotecario']} exact >
+					<ManterRelatorio />
+				</PrivateRoute>
+				<PrivateRoute path="/relatorio/devolucoes" authorize={['bibliotecario']} exact >
+					<RelatorioDevolucoesLivros />
+				</PrivateRoute>
+				<PrivateRoute path="/relatorio/emprestimos" authorize={['bibliotecario']} >
+					<RelatorioLivrosEmprestado />
+				</PrivateRoute>
+				<PrivateRoute path="/relatorio/emprestimo/:id" authorize={['bibliotecario']} >
+					<InformacoesDetalhadasEmprestimo />
+				</PrivateRoute>
 
 				{/* ---------------- Livro ---------------- */}
-				<Route path="/livro" component={ConsultarLivro} exact />
-				<Route path="/livro/cadastrar" component={CadastrarLivro} />
-				<Route path="/livro/atualizar/:id" component={AtualizarLivro} />
-			    <Route path="/livro" component={ConsultarLivro} exact />
-				<Route path="/livro/informacoes/:id" component={InformacoesDetalhadasConsulta} />
+				<PrivateRoute path="/livro" exact >
+					<ConsultarLivro />
+				</PrivateRoute>
+				<PrivateRoute path="/livro/cadastrar" authorize={['bibliotecario']} >
+					<CadastrarLivro />
+				</PrivateRoute>
+				<PrivateRoute path="/livro/atualizar/:id" authorize={['bibliotecario']} >
+					<AtualizarLivro />
+				</PrivateRoute>
+				<PrivateRoute path="/livro/informacoes/:id" >
+					<InformacoesDetalhadasConsulta />
+				</PrivateRoute>
 			</Switch>
 		</Router>
 	);
